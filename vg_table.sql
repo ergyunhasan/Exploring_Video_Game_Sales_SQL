0a463e4155a563a3d6124ae91fce9069d7ff0b02
Ergyun Hasan <121507597+ergyunhasan@users.noreply.github.com>
2023-05-24 16:11:38 +0200

    Add files via upload

---
+++ vg_table.sql
@@ -0,0 +1,22 @@
+DROP TABLE IF EXISTS bookings;
+
+# creating a table
+
+CREATE TABLE tracks (
+  Rank INT,
+  Name VARCHAR(255),
+  Platform VARCHAR(255),
+  Year INT,
+  Genre VARCHAR(255),
+  Publisher VARCHAR(255),
+  NA_Sales DECIMAL(10,2),
+  EU_Sales DECIMAL(10,2),
+  JP_Sales DECIMAL(10,2),
+  Other_Sales DECIMAL(10,2),
+  Global_Sales DECIMAL(10,2),
+  Critic_Score INT,
+  Critic_Count INT,
+  User_Score DECIMAL(3,1),
+  User_Count INT,
+  Rating VARCHAR(255)
+);
